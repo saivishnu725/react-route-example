58e95999edd1fdb925c1709bedb248c5a017ecd4
saivishnu725 <saivishnu725@gmail.com>
2024-04-26 20:37:20 +0530

    Removed reportWebVitals

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ import { React, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import MultiPage from './MultiPage';
-import reportWebVitals from './reportWebVitals';
-//import { useEffect } from 'react';
-
 
 export function useTitle(title) {
   useEffect(() => {
@@ -20,11 +17,9 @@ export function useTitle(title) {
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <MultiPage />
-  /*<React.StrictMode>
-  </React.StrictMode> */
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
